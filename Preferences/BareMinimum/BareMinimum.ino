a042a89fb0a34150b6838549813c65b8d55c07a3
Phi Luu <philectron@gmail.com>
2016-08-13 20:25:56 -0700

    Change Comment Style Preference

--- Preferences/BareMinimum/BareMinimum.ino
+++ Preferences/BareMinimum/BareMinimum.ino
@@ -1,26 +1,26 @@
-/*****************************************************************************
-
-Project Name: {Name of Project}
-
-File Name: {Name of File}.ino
-
-Description: {Type Desciption Here}
-
-Compatibility: Arduino {Type}
-
-Phi Luu
-Portland, Oregon, United States
-Created {Date}
-Updated {Date}
-
-*****************************************************************************/
+//****************************************************************************
+//
+// Project Name: {Name of Project}
+//
+// File Name: {Name of File}.ino
+//
+// Description: {Type Desciption Here}
+//
+// Compatibility: Arduino {Type}
+//
+// Phi Luu
+// Portland, Oregon, United States
+// Created {Date}
+// Updated {Date}
+//
+//****************************************************************************
 
 //**********setup**********
 // Put setup code here, to run once
 // Inputs: None
 // Outputs: None
 void setup() {
-    
+
 }
 
 //**********loop**********
@@ -28,5 +28,5 @@ void setup() {
 // Inputs: None
 // Outputs: None
 void loop() {
-    
+
 }
