ade45275f840d740ae2999adc413f42d3e06edda
Phi Luu <philectron@gmail.com>
2017-01-09 21:35:21 -0800

    Change capitalization of the folder - Move back in
    GitHub see "Preferences" and "preferences" as the same name, so it
    doesn't consider that as a change. First, move the folder to elsewhere
    outside of the repo, then change the name. Next, move it back to the
    repo.

---
+++ preferences/BareMinimum/BareMinimum.ino
@@ -0,0 +1,53 @@
+//****************************************************************************
+//
+// Project Name: Name_of_Project
+//
+// File Name: Name_of_File.ino
+//
+// Description: File_Description
+//
+// Compatibility: Arduino Arduino_Type
+//
+// Phi Luu
+// Portland, Oregon, United States
+// Created Created_Date
+// Updated Updated_Date
+//
+//****************************************************************************
+
+//***
+// Preprocessor Directives
+//***
+
+
+
+//***
+// Global declaration
+//***
+
+// Required hardware I/O connections
+
+
+// Global constants
+
+
+// Global variables
+
+
+//***
+// Subroutines
+//***
+
+
+
+//***
+// Mandatory Routines
+//***
+
+void setup() {
+    
+}
+
+void loop()  {
+    
+}
